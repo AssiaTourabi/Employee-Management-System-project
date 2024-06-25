c6ea4debbd710b2145949b07a648423bda41a59d
unknown <assiatourabi55@gmail.com>
2024-06-25 23:05:36 +0100

    modifications

--- EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d628eac7c16aa9158e052d041f0a8a4eee8c1ecc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077c750af7999c3f3017dfd708ec5c0fd98e6a29")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
